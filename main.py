c940039ea8fc6778eb8690ad40d33024b8a08ae5
eugene_zaharch <eugenezaharchenko@gmail.com>
2024-12-17 17:17:29 +0200

    calc function added

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+from typing import Optional
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse, Response, JSONResponse
 
 app = FastAPI()
 
@@ -6,6 +9,20 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/calc")
+async def calculate(x: int, y: int, z: Optional[int] = None):
+    '''http://localhost:8000/calc?x=1&y=3&z=0'''
+    if z == 0:
+        return JSONResponse(content={"ERROR": "Z cannot be zero"},
+                            status_code=400)
+        # return Response(content='{"ERROR": "Z cannot be zero"}',
+        #                 media_type='application/json', status_code=400)
+    if z is None:
+        z = 1
+    result = (x + y) / z
+    return HTMLResponse(f"<h1>{result}</h1>")
 
 
 @app.get("/hello/{name}")
